660ba59aa7d5e1fac8b8b94463f09caf2b70d4cc
James <james.m.overholt@gmail.com>
2023-10-31 12:36:08 -0400

    Created and tested qt8

--- qt8.py
+++ qt8.py
@@ -2,7 +2,25 @@
 
 from djitellopy import Tello
 from balloonIdentifier import BalloonIdentifier
+import cv2
 
 tello = Tello()
 balloon = BalloonIdentifier()
 
+tello.connect(True)
+
+tello.streamon()
+
+tello.takeoff()
+
+while True:
+    frame = tello.get_frame_read().frame
+    if frame is not None:
+        balloonData = balloon.identifyBalloon(frame)
+        if balloonData is not None:
+            corners, ids, rejected, colors = balloonData
+            for i in range(len(ids)):
+                print("The balloon with marker", ids[i], "is", colors[i])
+            break
+    if (cv2.waitKey(1) == 27): break
+tello.land()
